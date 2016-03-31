895f5710e16f915b0c0cf1f22dc892f4dc00d257
lxx <listen987654321@sina.com>
2016-03-31 18:13:11 +0800

    Initial Commit

---
+++ Ry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ry
+//
+//  Created by lxx on 16/3/31.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
